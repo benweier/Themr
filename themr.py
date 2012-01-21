af894073719ed1dceff96c591edbed67033d14a3
Ben Weier <ben.weier@gmail.com>
2012-01-21 13:13:35 +1000

    improved flexibility and error checking

--- themr.py
+++ themr.py
@@ -1,15 +1,23 @@
 import sublime, sublime_plugin
 
-def get_theme(t):
-	s = sublime
-	t = s.load_settings("Global.sublime-settings").get("theme", t)
-	s.status_message(t)
+def get_theme(g):
+	return sublime.load_settings(g).get("theme")
 
-def set_theme(t):
+def set_theme(t, g):
 	s = sublime
-	s.load_settings("Global.sublime-settings").set("theme", t)
-	s.save_settings("Global.sublime-settings")
+	s.load_settings(g).set("theme", t)
+	s.save_settings(g)
+	if get_theme(g) == t:
+		s.status_message("Themr saved: " + t)
+		s.window.run_command("set_setting", {"setting": "theme", "value": t})
+	else:
+		s.error_message("Error saving theme. The read/write operation may have failed.")
 
 class ChangeThemeCommand(sublime_plugin.ApplicationCommand):
+	def __init__(self):
+		g = "Global.sublime-settings"
+		sublime.status_message("Themr: " + get_theme(g))
+
 	def run(self, t):
-		set_theme(t)
+		g = "Global.sublime-settings"
+		set_theme(t, g)
