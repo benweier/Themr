b9952183d9580d313d7fc909ed2ab547cc70aca9
Ben Weier <ben.weier@gmail.com>
2012-01-22 15:40:12 +1000

    remove theme check

--- themr.py
+++ themr.py
@@ -4,8 +4,9 @@ import os
 class ChangeThemeCommand(sublime_plugin.ApplicationCommand):
 	def __init__(self):
 		# TODO get list of theme files to build menu structure
+		# TODO add command pallete options to select a theme
 		self.list_themes()
-		# TODO EventListener for new .sublime-theme files to rebuild menu?
+		# TODO EventListener for new .sublime-theme files to dynamically rebuild menu?
 		self.settings = sublime.load_settings("Global.sublime-settings")
 		sublime.status_message("Themr: " + self.get_theme())
 
@@ -14,21 +15,17 @@ class ChangeThemeCommand(sublime_plugin.ApplicationCommand):
 
 	def list_themes(self):
 		# packages = os.listdir(sublime.packages_path())
-		# check for theme folders
 		return
 
 	def get_theme(self):
 		return self.settings.get("theme", "Default.sublime-theme")
 
 	def set_theme(self, t):
+		# TODO set_setting to update checkbox on selected theme
+		# run_command("set_setting", {"setting": "theme", "value": t})
+		self.settings.set("theme", t)
+		sublime.save_settings("Global.sublime-settings")
 		if self.get_theme() == t:
 			sublime.status_message("Themr: " + t)
-			# TODO set_setting to update checkbox on selected theme
-			# run_command("set_setting", {"setting": "theme", "value": t})
 		else:
-			self.settings.set("theme", t)
-			sublime.save_settings("Global.sublime-settings")
-			if self.get_theme() == t:
-				sublime.status_message("Themr: " + t)
-			else:
-				sublime.error_message("Error saving theme. The read/write operation may have failed.")
+			sublime.status_message("Error saving theme. The read/write operation may have failed.")
