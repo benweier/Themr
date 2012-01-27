c9e5dd6ac1d3488a9112fe2dcd5108de79809e5b
Ben Weier <ben.weier@gmail.com>
2012-01-27 21:06:06 +1000

    b0.7

--- themr.py
+++ themr.py
@@ -1,5 +1,8 @@
 import sublime, sublime_plugin
 import os
+import json
+
+themr = os.getcwd()
 
 class SwitchThemeCommand(sublime_plugin.ApplicationCommand):
 	def __init__(self):
@@ -7,7 +10,9 @@ class SwitchThemeCommand(sublime_plugin.ApplicationCommand):
 		# TODO use command pallete to run a theme menu rebuild
 		self.settings = sublime.load_settings('Global.sublime-settings')
 		sublime.status_message('Themr: ' + self.get_theme())
+		
 		self.themes = self.list_themes()
+		self.build_theme_data()
 
 	def run(self, t):
 		if self.get_theme() != t:
@@ -18,12 +23,23 @@ class SwitchThemeCommand(sublime_plugin.ApplicationCommand):
 		packages = os.listdir(sublime.packages_path())
 
 		for package in (package for package in packages if package.startswith('Theme -')):
-			theme = os.listdir(sublime.packages_path() + "\\" + package)
+			theme = os.listdir(sublime.packages_path() + '\\' + package)
 
 			for filename in (filenames for filenames in theme if filenames.endswith('.sublime-theme')):
 				themes.append(filename)
 
 		return themes
+
+	def build_theme_data(self):
+		data = []
+
+		for theme in self.themes:
+			data.append({'caption': 'Themr: ' + os.path.splitext(theme)[0], 'command': 'switch_theme', 'args': { 't': theme }})
+			commands = json.dumps(data, indent = 4)
+
+		f = open(themr + "\\" + "Default.sublime-commands", 'w')
+		f.write(commands + "\n")
+		f.close
 
 	def get_theme(self):
 		return self.settings.get('theme', 'Default.sublime-theme')
