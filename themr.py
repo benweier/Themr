e1f434341e2914598c694ab8667985e66d69558f
Ben Weier <ben.weier@gmail.com>
2013-06-11 15:41:41 +1000

    Fixed Default theme appearing twice in ST2. Function changes for upcoming feature

--- themr.py
+++ themr.py
@@ -3,8 +3,10 @@ import os, zipfile
 from random import random
 
 class Themr():
+	preferences = 'Preferences.sublime-settings'
+
 	def load_themes(self):
-		themes = [["Theme: Default", "Default.sublime-theme"]]
+		themes = []
 
 		for root, dirs, files in os.walk(sublime.packages_path()):
 			for filename in (filename for filename in files if filename.endswith('.sublime-theme')):
@@ -16,46 +18,47 @@ class Themr():
 				zf = zipfile.ZipFile(os.path.join(sublime.installed_packages_path(), package))
 				for filename in (filename for filename in zf.namelist() if filename.endswith('.sublime-theme')):
 					name = os.path.basename(filename).replace('.sublime-theme', '')
-					themes.append(['Theme: ' + name, filename])
+					themes.append(["Theme: " + name, filename])
+
+		default_theme = os.path.join(os.getcwd(), 'Packages', 'Theme - Default.sublime-package')
+		if os.path.exists(default_theme):
+			themes.append(["Theme: Default", "Default.sublime-theme"])
 
 		themes.sort()
 		return themes
 
-	def set_theme(self, t):
-		self.settings().set('theme', t)
-		sublime.save_settings('Preferences.sublime-settings')
-
-	def get_theme(self):
-		return self.settings().get('theme')
-
-	def cycle_theme(self, d):
+	def cycle_theme(self, direction):
 		themes = self.load_themes()
-		the_theme = self.get_theme()
+		the_theme = self.load('theme', 'Default.sublime-theme')
 		index = 0
 		the_index = [theme[1] for theme in themes].index(the_theme)
 		num_of_themes = len(themes)
 
-		if d == "next":
+		if direction == 'next':
 			index = the_index + 1 if the_index < num_of_themes - 1 else 0
 
-		if d == "prev":
+		if direction == 'prev':
 			index = the_index - 1 if the_index > 0 else num_of_themes - 1
 
-		if d == "rand":
+		if direction == 'rand':
 			index = int(random() * len(themes))
 
-		self.set_theme(themes[index][1])
+		self.save('theme', themes[index][1])
 		sublime.status_message(themes[index][0])
 
-	def settings(self):
-		return sublime.load_settings('Preferences.sublime-settings')
+	def save(self, setting, value):
+		sublime.load_settings(self.preferences).set(setting, value)
+		sublime.save_settings(self.preferences)
+
+	def load(self, setting, default):
+		return sublime.load_settings(self.preferences).get(setting, default)
 
 Themr = Themr()
 
 class ThemrListThemesCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		themes = Themr.load_themes()
-		the_theme = Themr.get_theme()
+		the_theme = Themr.load('theme', 'Default.sublime-theme')
 		try:
 			the_index = [theme[1] for theme in themes].index(the_theme)
 		except (ValueError):
@@ -63,25 +66,24 @@ class ThemrListThemesCommand(sublime_plugin.WindowCommand):
 
 		def on_done(index):
 			if index != -1:
-				Themr.set_theme(themes[index][1])
-				sublime.status_message(themes[index][0])
+				Themr.save('theme', themes[index][1])
 
 			if index == -1:
-				Themr.set_theme(themes[the_index][1])
+				Themr.save('theme', themes[the_index][1])
 
-		def on_select(index):
-			Themr.set_theme(themes[index][1])
-
-		self.window.show_quick_panel(themes, on_done)
+		try:
+			self.window.show_quick_panel(themes, on_done, 0, the_index)
+		except:
+			self.window.show_quick_panel(themes, on_done)
 
 class ThemrNextThemeCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		Themr.cycle_theme("next")
+		Themr.cycle_theme('next')
 
 class ThemrPreviousThemeCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		Themr.cycle_theme("prev")
+		Themr.cycle_theme('prev')
 
 class ThemrRandomThemeCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		Themr.cycle_theme("rand")
+		Themr.cycle_theme('rand')
