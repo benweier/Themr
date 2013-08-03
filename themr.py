b7ebf57cd499788ee12f8889ae6c1e253fbe8100
Ben Weier <ben.weier@gmail.com>
2013-08-03 16:29:42 +1000

    initial work for supporting favourites

--- themr.py
+++ themr.py
@@ -3,47 +3,43 @@ import os, zipfile
 from random import random
 
 class Themr():
-	preferences = 'Preferences.sublime-settings'
+	preferences = sublime.load_settings('Preferences.sublime-settings')
 
 	def load_themes(self):
-		themes = []
+		all_themes = []
 
 		try: # use find_resources() first for ST3
 			for theme_resource in sublime.find_resources("*.sublime-theme"):
 				filename = os.path.basename(theme_resource)
-				name = filename.replace('.sublime-theme', '')
-				themes.append(['Theme: ' + name, filename])
+				all_themes.append(filename)
 
 		except: # fallback to walk() for ST2
 			for root, dirs, files in os.walk(sublime.packages_path()):
 				for filename in (filename for filename in files if filename.endswith('.sublime-theme')):
-					name = filename.replace('.sublime-theme', '')
-					themes.append(['Theme: ' + name, filename])
+					all_themes.append(filename)
 
 			for root, dirs, files in os.walk(sublime.installed_packages_path()):
 				for package in (package for package in files if package.endswith('.sublime-package')):
 					zf = zipfile.ZipFile(os.path.join(sublime.installed_packages_path(), package))
 					for filename in (filename for filename in zf.namelist() if filename.endswith('.sublime-theme')):
-						name = os.path.basename(filename).replace('.sublime-theme', '')
-						themes.append(["Theme: " + name, filename])
+						all_themes.append(filename)
+
+		favorite_themes = self.preferences.get('themr_favorites', [])
+		themes = []
+
+		for theme in all_themes:
+			favorited = theme in favorite_themes
+			pretty_name = 'Theme: ' + theme.replace('.sublime-theme', '')
+			if favorited: pretty_name += u' \N{BLACK STAR}' # Put a pretty star icon next to favorited themes. :)
+			themes.append([pretty_name, theme, favorited])
 
 		themes.sort()
 		return themes
 
-	def save(self, setting, value):
-		sublime.load_settings(self.preferences).set(setting, value)
-		sublime.save_settings(self.preferences)
-
-	def load(self, setting, default):
-		return sublime.load_settings(self.preferences).get(setting, default)
-
-Themr = Themr()
-
-class ThemrListThemesCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		themes = Themr.load_themes()
-		the_theme = Themr.load('theme', 'Default.sublime-theme')
-		allow_preview = Themr.load('themr_allow_preview', False)
+	def list_themes(self, window, theme_list):
+		themes = [[theme[0], theme[1]] for theme in theme_list]
+		the_theme = Themr.get('theme', 'Default.sublime-theme')
+		allow_preview = Themr.get('themr_allow_preview', False)
 		self.user_selected = False
 		try:
 			the_index = [theme[1] for theme in themes].index(the_theme)
@@ -52,24 +48,37 @@ class ThemrListThemesCommand(sublime_plugin.WindowCommand):
 
 		def on_done(index):
 			if index != -1:
-				Themr.save('theme', themes[index][1])
+				Themr.set('theme', themes[index][1])
 				sublime.status_message(themes[index][0])
 
 		def on_select(index):
 			if self.user_selected == True and allow_preview == True:
-				Themr.save('theme', themes[index][1])
+				self.preferences.set('theme', themes[index][1]) # use ST's set() so the theme "preview" is not saved to file
 			else:
 				self.user_selected = True
 
 		try:
-			self.window.show_quick_panel(themes, on_done, 0, the_index, on_select)
+			window.show_quick_panel(themes, on_done, 0, the_index, on_select)
 		except:
-			self.window.show_quick_panel(themes, on_done)
+			window.show_quick_panel(themes, on_done)
+
+	def set(self, setting, value):
+		self.preferences.set(setting, value)
+		sublime.save_settings('Preferences.sublime-settings')
+
+	def get(self, setting, default):
+		return self.preferences.get(setting, default)
+
+Themr = Themr()
+
+class ThemrListThemesCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		Themr.list_themes(self.window, Themr.load_themes())
 
 class ThemrCycleThemesCommand(sublime_plugin.WindowCommand):
 	def run(self, direction):
 		themes = Themr.load_themes()
-		the_theme = Themr.load('theme', 'Default.sublime-theme')
+		the_theme = Themr.get('theme', 'Default.sublime-theme')
 		index = 0
 		the_index = [theme[1] for theme in themes].index(the_theme)
 		num_of_themes = len(themes)
@@ -83,7 +92,7 @@ class ThemrCycleThemesCommand(sublime_plugin.WindowCommand):
 		if direction == 'rand':
 			index = int(random() * len(themes))
 
-		Themr.save('theme', themes[index][1])
+		Themr.set('theme', themes[index][1])
 		sublime.status_message(themes[index][0])
 
 class ThemrNextThemeCommand(sublime_plugin.WindowCommand):
