7e7a53b9b4993c0711f82a3e1d3a35cd7d715c3a
Ben Weier <ben.weier@gmail.com>
2014-11-17 09:06:36 +1000

    Use correct reference to Themr instance instead of self #9

--- themr.py
+++ themr.py
@@ -231,7 +231,7 @@ class ThemrToggleSettingsCommand(sublime_plugin.WindowCommand):
 
 		# Toggles the setting key/val at the index specified
 		def on_done(index):
-			self.preferences.set(the_theme_settings[index][0], not the_theme_settings[index][1])
+			Themr.instance().preferences.set(the_theme_settings[index][0], not the_theme_settings[index][1])
 			sublime.save_settings(PREFERENCES)
 
 			if the_theme_settings[index][1]:
